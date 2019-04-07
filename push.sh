69b6c024e3b4eb635b39b34267ab3aa25b7410b6
Christos Sakoumpentas <c.sakoumpentas@protonmail.com>
2019-04-07 15:18:42 +0300

    Update push.sh

--- push.sh
+++ push.sh
@@ -10,8 +10,8 @@ commit_pdf() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/csakou/resume.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git remote add origin-pages https://${GH_TOKEN}@github.com/csakou/resume.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages master gh-pages
 }
 
 setup_git
