8b4c675bec4d4f28ebf5e8f4cdec6917f953a606
Christos Sakoumpentas <c.sakoumpentas@protonmail.com>
2019-04-07 17:11:56 +0300

    Update push.sh

--- push.sh
+++ push.sh
@@ -9,9 +9,10 @@ commit_pdf() {
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
-upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/csakou/resume.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+upload_files() 
+  git remote remove origin
+  git remote add origin https://${GH_TOKEN}@github.com/csakou/resume.git > /dev/null 2>&1
+  git push --quiet -u origin master
 }
 
 setup_git
