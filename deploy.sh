2a8f440d30142e329f4d55958b3dea02e55c051f
Christos Sakoumpentas <c.sakoumpentas@protonmail.com>
2019-04-07 13:22:27 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-./pandoc-2.6/bin/pandoc https://github.com/csakou/resume/blob/master/_config.yml -o resume.pdf --template=https://github.com/csakou/resume/blob/master/template.tex --pdf-engine=xelatex
+./pandoc-2.6/bin/pandoc _config.yml -o resume.pdf --template=template.tex --pdf-engine=xelatex
